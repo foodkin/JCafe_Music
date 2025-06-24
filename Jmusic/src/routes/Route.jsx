96237f3728f32fa5d4795ebea9051dbaa419f46c
foodkin <jasonjeferson81@gmail.com>
2025-06-25 02:21:22 +0700

    Update Route.jsx

--- Jmusic/src/routes/Route.jsx
+++ Jmusic/src/routes/Route.jsx
@@ -1,14 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 
 import Layout from "../components/Layout";
-import Home from "../pages/Home";
-import FinalProjeck from "../pages/FinalProject";
+import Home from "../Pages/Home";
+import FinalProjeck from "../Pages/FinalProject";
 
 // Generation pages
-import GenRouter from "../pages/Gen/GenRouter"; // ✅ Router untuk pagination
-import Gen12 from "../pages/Gen/Gen12";
-import Gen13 from "../pages/Gen/Gen13";
-import Gen14 from "../pages/Gen/Gen14";
+import GenRouter from "../Pages/Gen/GenRouter"; // ✅ Router untuk pagination
+import Gen12 from "../Pages/Gen/Gen12";
+import Gen13 from "../Pages/Gen/Gen13";
+import Gen14 from "../Pages/Gen/Gen14";
 
 function RouteList() {
   return (
