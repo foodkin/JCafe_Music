e5b41e85e1377e97611ceef2c018a00509011658
foodkin <jasonjeferson81@gmail.com>
2025-06-25 02:18:51 +0700

    Change 4

--- Jmusic/vite.config.js
+++ Jmusic/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
+  root: '.', // ini penting kalau kamu taruh vite.config.js di dalam Jmusic
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html'),
+    },
+  },
 })
